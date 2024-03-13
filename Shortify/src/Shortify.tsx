132996fb00fd442995ac352e7718d83b97c552a8
krisk <krishnaprasad.1.k@nokia.com>
2024-03-13 18:31:38 +0530

    Format changes

--- Shortify/src/Shortify.tsx
+++ Shortify/src/Shortify.tsx
@@ -1,31 +1,32 @@
 // import 'dotenv/config';
-import { useState } from 'react';
-import { Button, TextField, Typography, Container } from '@material-ui/core';
+import { useState } from "react";
+import { Button, TextField, Typography, Container } from "@material-ui/core";
 
 const Shortify = () => {
-  const [originalUrl, setOriginalUrl] = useState('');
-  const [shortUrl, setShortUrl] = useState('');
+  const [originalUrl, setOriginalUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
 
   // console.log(process.env)
 
-  const handleShorten = async () => {
-    // Implement your logic to send a POST request to your server to shorten the URL
-    // Update the shortUrl state with the result
-    // For simplicity, let's assume the server returns a shortened URL directly
-    const response = await fetch("http://localhost:3000/shorten", {
-      method: 'POST',
-      mode: 'no-cors',      
-      body: JSON.stringify({ "url": "https://github.com/krishnakodoth/Express/tree/main/short-url"}),
-    });
-
-    // const result = await response.json();
-    const result = await response;
-    console.log(result)
-    // setShortUrl(result.shortUrl);
+  const handleShorten = () => {
+    fetch("http://localhost:3000/shorten", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: "https://github.com/krishnakodoth/Express/tree/main/short-url",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setShortUrl("http://localhost:3000/shorten/"+data.shortUrl)
+      })
+      .catch((error) => console.error("Error:", error));
   };
 
   return (
-    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '50px' }}>
+    <Container maxWidth="sm" style={{ textAlign: "center", marginTop: "50px" }}>
       <Typography variant="h4" gutterBottom>
         Shortify - URL Shortener
       </Typography>
@@ -41,7 +42,7 @@ const Shortify = () => {
         Shorten
       </Button>
       {shortUrl && (
-        <div style={{ marginTop: '20px' }}>
+        <div style={{ marginTop: "20px" }}>
           <Typography variant="subtitle1">Shortened URL:</Typography>
           <a href={shortUrl} target="_blank" rel="noopener noreferrer">
             {shortUrl}
